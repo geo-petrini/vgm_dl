385eab438b9275e08b5e7115226ebd920b06f634
geo-petrini <geo.petrini@gmail.com>
2024-04-12 15:45:38 +0200

    album minor restyling

--- static/js/index.js
+++ static/js/index.js
@@ -42,7 +42,7 @@ function createAlbumDivs(albums) {
 function updateAlbum(album) {
     // Update existing element if content differs
     var existingElement = albumMap[album.id];
-    var existingContent = existingElement;
+    // var existingContent = existingElement;
     var existingContent = existingElement.html();
     var newContent = createAlbumContent(album);
     // console.log(`existingContent: ${existingContent}`)
@@ -69,18 +69,35 @@ function createAlbumContent(album) {
     // extra div outside the card div because it will be removed by the .html() function
     // using .html() to serialize the data the same way as it will be loaded in updateAlbum() for confrontation
     var html = $(`<div>
-    <div class="card border-dark mb-3">
-        <div class="row g-0">
-            <div class="col">
-                <div class="card-body">
-                    <img src="data:image/jpeg;base64,${album.thumbnail}" class="img-fluid rounded">
+    <div id="${album.id}_content">
+        <div class="card border-dark mb-3">
+            <div class="row g-0">
+                <div class="col">
+                    <div class="card-body">
+                        <img src="data:image/jpeg;base64,${album.thumbnail}" class="img-fluid rounded">
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-10">
-                <div class="card-body">
-                    <h5 class="card-title">${album.title}</h5>
-                    <p class="card-text">${album.status}</p>
-                    <a href="${album.url}" target="_blank" class="btn btn-link">${album.url}</a>
+                <div class="col-md-10">
+                    <div class="card-body">
+                        <h5 class="card-title">${album.title}</h5>
+                        <p class="card-text">${album.status}</p>
+                        <div class="btn-group" role="group">
+                        <button type="button" class="btn btn-primary disabled text-nowrap">Download album</button>
+                        <a class="btn btn-primary disabled" role="button" href="download/${album.id}" target="_blank">
+                            <!-- mdi:theme -->
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M13 5v6h1.17L12 13.17L9.83 11H11V5zm2-2H9v6H5l7 7l7-7h-4zm4 15H5v2h14z"/></svg>
+                        </a>
+                    </div>   
+                        <div class="btn-group" role="group">
+                            <button type="button" class="btn btn-secondary disabled text-nowrap">Album URL</button>
+                            <a class="btn btn-secondary" role="button" href="${album.url}" target="_blank">
+                                <!-- mdi:theme -->
+                                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M14 3v2h3.59l-9.83 9.83l1.41 1.41L19 6.41V10h2V3m-2 16H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7h-2z"/></svg>                            
+                            </a>
+                        </div>
+
+
+                    </div>
                 </div>
             </div>
         </div>
@@ -88,6 +105,55 @@ function createAlbumContent(album) {
     </div>
     `).html()
     return html
+}
+
+function createTracksList(album){
+    var html = $(`<div>
+    <ul id="${album.id}_tracks" class="list-group">
+    </ul>   
+    </div>
+    `).html()
+    return html 
+}
+
+function createTrackListItem(track){
+    html = $(`
+    <li id="5f4f9440773948afa159e557bcc3233d" class="list-group-item">
+        01 Crystal Pier 
+        <span class="badge text-bg-success">mp3</span>
+        <div class="btn-group" role="group">
+            <button type="button" class="btn btn-primary disabled text-nowrap">Download</button>
+            <a class="btn btn-primary" role="button" href="downloads\\\\Dorfromantik Soundtrack Vol. 2\\\\flac\\\\01 Crystal Pier.flac" target="_blank">
+                <!-- mdi:theme -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M13 5v6h1.17L12 13.17L9.83 11H11V5zm2-2H9v6H5l7 7l7-7h-4zm4 15H5v2h14z"/></svg>
+            </a>
+        </div>                                
+        <div class="btn-group" role="group">
+            <button type="button" class="btn btn-secondary disabled text-nowrap">Track URL</button>
+            <a class="btn btn-secondary" role="button" href="https://epsilon.vgmsite.com/soundtracks/dorfromantik-soundtrack-vol.-2-2022/yyduovwddt/01%20Crystal%20Pier.flac" target="_blank">
+                <!-- mdi:theme -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M14 3v2h3.59l-9.83 9.83l1.41 1.41L19 6.41V10h2V3m-2 16H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7h-2z"/></svg>                            
+            </a>
+        </div>                                
+    </li>
+    `)
+    return html
+}
+
+function createTracksTable(album){
+`    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Actions</th>
+                <th scope="col">Size</th>
+                <th scope="col">Status</th>
+            </tr>
+        </thead>
+        <tbody class="table-group-divider">
+    §   </tbody>
+    </table>`
+
 }
 
 function fetchTracks() {
